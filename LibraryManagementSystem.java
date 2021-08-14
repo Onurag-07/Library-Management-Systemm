7030395e9a104be0572ef51f8e4d43fe08155af3
Aman Anurag <70232721+Onurag-07@users.noreply.github.com>
2021-08-14 13:13:23 +0530

    Add files via upload

---
+++ LibraryManagementSystem.java
@@ -0,0 +1,63 @@
+
+/*
+            TCR  Innovation
+   CORE JAVA+SQL INTERNSHIP PROGRAM
+        INTERNSHIP (( FINAL PROJECT ON LIBRARY MANAGEMENT SYSTEM )) .
+
+
+INTERNSHIP (( FINAL PROJECT ON LIBRARY MANAGEMENT SYSTEM ))
+
+ */
+
+package library.management.system;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LibraryManagementSystem extends JFrame implements ActionListener{
+
+        JLabel l1;
+        JButton b1;
+        
+        public LibraryManagementSystem() {
+		super("Library Management System");
+                setSize(1366,390);
+                setLayout(null);
+                setLocation(300,300);
+                
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		l1 = new JLabel("");
+                b1 = new JButton("Next");
+                b1.setFont(new Font("Tahoma", Font.PLAIN, 35));
+                b1.setBackground(Color.WHITE);
+                b1.setForeground(Color.BLACK);
+				
+                
+                
+                ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("library/management/system/icons/library.gif"));
+                Image i3 = i1.getImage().getScaledInstance(1366, 390,Image.SCALE_DEFAULT);
+                ImageIcon i2 = new ImageIcon(i3);
+                l1 = new JLabel(i2);
+                
+                b1.setBounds(1050,250,200,60);
+		l1.setBounds(0, 0, 1366, 390);
+                
+                l1.add(b1);
+		add(l1);
+                
+                b1.addActionListener(this);
+	}
+        
+        public void actionPerformed(ActionEvent ae){
+                new Login_user().setVisible(true);
+                this.setVisible(false);
+                
+        }
+        
+        public static void main(String[] args) {
+                LibraryManagementSystem window = new LibraryManagementSystem();
+                window.setVisible(true);			
+	}
+}
